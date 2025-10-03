bb4410b6b7f7d77515628bf0c749e1926f091bbe
Jorge Ubaldo <1231274@isep.ipp.pt>
2025-10-03 13:01:26 +0100

    [LOGS - #123] Log system implementation (each namespace have each logs/docs)

--- SEM5-PI-WEBAPI/Program.cs
+++ SEM5-PI-WEBAPI/Program.cs
@@ -1,14 +1,51 @@
+using Serilog;
+
 namespace SEM5_PI_WEBAPI
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                CreateLogsConfigurations();
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch(Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static void CreateLogsConfigurations()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .WriteTo.File("Logs/GeneralLogs/general-.log", rollingInterval: RollingInterval.Day)
+
+                .WriteTo.Logger(lc => lc
+                    .Filter.ByIncludingOnly(e=>
+                        e.Properties.ContainsKey("SourceContext") &&
+                        e.Properties["SourceContext"].ToString().Contains("SEM5_PI_WEBAPI.Domain.VesselsTypes"))
+                    .WriteTo.File("Logs/VesselsTypes/vesseltype-.log",
+                        rollingInterval: RollingInterval.Day,
+                        outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext} - {Message:lj}{NewLine}{Exception}")
+                )
+
+                .CreateLogger();
+
+
+            Log.Information("Starting web host");
         }
         
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
