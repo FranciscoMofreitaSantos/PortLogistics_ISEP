9ccf07c4b1b728258dc04e7754e5e876b6e03cb2
Francisco Santos <1230564@isep.ipp.pt>
2025-10-07 23:24:08 +0100

    Cargo Manifest Controller added
    Issue: #15

---
+++ SEM5-PI-WEBAPI/Controllers/CargoManifestController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using SEM5_PI_WEBAPI.Domain.CargoManifests;
+using SEM5_PI_WEBAPI.Domain.Shared;
+
+namespace SEM5_PI_WEBAPI.Controllers;
+
+[Microsoft.AspNetCore.Components.Route("api/[controller]")]
+[ApiController]
+public class CargoManifestController : ControllerBase
+{
+    private readonly CargoManifestService _service;
+
+    public CargoManifestController(CargoManifestService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CargoManifestDto>>> GetAll()
+    {
+        return await _service.GetAllAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<CargoManifestDto>> GetById(Guid id)
+    {
+        var cargo = await _service.GetByIdAsync(new CargoManifestId(id));
+
+        if (cargo == null)
+            return NotFound();
+
+        return cargo;
+    }
+    
+    
+    [HttpPost]
+    public async Task<ActionResult<CargoManifestDto>> Create(CreatingCargoManifestDto dto)
+    {
+        try
+        {
+            var cargo = await _service.AddAsync(dto);
+
+            return CreatedAtAction(nameof(GetById), new { id = cargo.Id }, cargo);
+        }
+        catch (BusinessRuleValidationException ex)
+        {
+            return BadRequest(new { Message = ex.Message });
+        }
+    }
+    
+}
