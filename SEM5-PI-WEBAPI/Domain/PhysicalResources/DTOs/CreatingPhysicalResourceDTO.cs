957690f211da325f287ad2e8743ec3e2ebd5f170
Emanuel Almeida <1230839@isep.ipp.pt>
2025-10-10 17:48:58 +0100

    US 2.2.12 - Implementation - Issue #109/#113] PhysicalResource DTOs created.

---
+++ SEM5-PI-WEBAPI/Domain/PhysicalResources/DTOs/CreatingPhysicalResourceDTO.cs
@@ -0,0 +1,28 @@
+﻿using SEM5_PI_WEBAPI.Domain.BusinessShared;
+using SEM5_PI_WEBAPI.Domain.Qualifications;
+
+namespace SEM5_PI_WEBAPI.Domain.PhysicalResources.DTOs;
+
+public class CreatingPhysicalResourceDTO
+{
+    public string Code { get; set; }
+    public string Description { get; set; }
+    public double OperationalCapacity { get; set; }
+    public double SetupTime { get; set; }
+    public PhysicalResourceType PhysicalResourceType { get; set; }
+    public PhysicalResourceStatus PhysicalResourceStatus { get; set; }
+    public Guid? QualificationID { get; set; }
+
+    public CreatingPhysicalResourceDTO(string code, string description, double operationalCapacity,
+        double setupTime, PhysicalResourceType physicalResourceType, PhysicalResourceStatus physicalResourceStatus,
+        Guid? qualificationID)
+    {
+        Code = code;
+        Description = description;
+        OperationalCapacity = operationalCapacity;
+        SetupTime = setupTime;
+        PhysicalResourceType = physicalResourceType;
+        PhysicalResourceStatus = physicalResourceStatus;
+        QualificationID = qualificationID;
+    }
+}
---
+++ SEM5-PI-WEBAPI/Domain/PhysicalResources/DTOs/PhysicalResourceDTO.cs
@@ -0,0 +1,37 @@
+﻿using SEM5_PI_WEBAPI.Domain.BusinessShared;
+using SEM5_PI_WEBAPI.Domain.Qualifications;
+
+namespace SEM5_PI_WEBAPI.Domain.PhysicalResources.DTOs;
+
+public class PhysicalResourceDTO
+{
+    public Guid Id { get; set; }
+    public string Code { get; set; }
+    public string Description { get; set; }
+    public double OperationalCapacity { get; set; }
+    public double SetupTime { get; set; }
+    public PhysicalResourceType PhysicalResourceType { get; set; }
+    public PhysicalResourceStatus PhysicalResourceStatus { get; set; }
+    public Guid? QualificationID { get; set; }
+    
+    
+    public PhysicalResourceDTO(
+        Guid id,
+        string code,
+        string description,
+        double operationalCapacity,
+        double setupTime,
+        PhysicalResourceType physicalResourceType,
+        PhysicalResourceStatus physicalResourceStatus,
+        Guid? qualificationID)
+    {
+        Id = id;
+        Code = code;
+        Description = description;
+        OperationalCapacity = operationalCapacity;
+        SetupTime = setupTime;
+        PhysicalResourceType = physicalResourceType;
+        PhysicalResourceStatus = physicalResourceStatus;
+        QualificationID = qualificationID;
+    }
+}
