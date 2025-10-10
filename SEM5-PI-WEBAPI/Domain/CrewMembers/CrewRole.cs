3500d7dc97755d1d0176d2c1730c32403878f654
Francisco Santos <1230564@isep.ipp.pt>
2025-10-10 19:43:41 +0100

    Add CrewMember domain class with name validation and enums for Nationality and CrewRole
    Issue: #15

---
+++ SEM5-PI-WEBAPI/Domain/CrewMembers/CrewRole.cs
@@ -0,0 +1,25 @@
+namespace SEM5_PI_WEBAPI.Domain.CrewMembers;
+
+public enum CrewRole
+{
+    Captain,
+    ChiefOfficer,
+    SecondOfficer,
+    ThirdOfficer,
+    ChiefEngineer,
+    SecondEngineer,
+    ThirdEngineer,
+    FourthEngineer,
+    Electrician,
+    Bosun,
+    AbleSeaman,
+    OrdinarySeaman,
+    Cook,
+    Steward,
+    DeckCadet,
+    EngineCadet,
+    RadioOfficer,
+    SafetyOfficer,
+    Purser,
+    ChiefCook
+}
