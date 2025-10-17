f8fd3413acd3d8662a1a247d3a0c87b50ed2f7e2
Emanuel Almeida <1230839@isep.ipp.pt>
2025-10-17 11:10:55 +0100

    US 2.2.12 - Implementation - Issue #109/#113] PhysicalResourceControllerTest.cs added.

--- SEM5-PI-WEBAPI.Tests/Controllers/PhysicalResourceControllerTest.cs
+++ SEM5-PI-WEBAPI.Tests/Controllers/PhysicalResourceControllerTest.cs
@@ -12,6 +12,221 @@ namespace SEM5_PI_WEBAPI.Tests.Controllers
 {
     public class PhysicalResourceControllerTests
     {
-       
+        private readonly Mock<IPhysicalResourceService> _serviceMock;
+        private readonly PhysicalResourceController _controller;
+
+        public PhysicalResourceControllerTests()
+        {
+            _serviceMock = new Mock<IPhysicalResourceService>();
+            _controller = new PhysicalResourceController(_serviceMock.Object);
+        }
+
+        private PhysicalResourceDTO CreateDTO()
+        {
+            return new PhysicalResourceDTO(
+                Guid.NewGuid(),
+                new PhysicalResourceCode("TRUCK-0001"),
+                "Truck A",
+                25.0,
+                10.0,
+                PhysicalResourceType.Truck,
+                PhysicalResourceStatus.Available,
+                Guid.NewGuid());
+        }
+
+        // --- GET ALL ---
+        [Fact]
+        public async Task GetAll_ShouldReturnOk_WhenResourcesExist()
+        {
+            var list = new List<PhysicalResourceDTO> { CreateDTO() };
+            _serviceMock.Setup(s => s.GetAllAsync()).ReturnsAsync(list);
+
+            var result = await _controller.GetAll();
+
+            Assert.NotNull(result.Value);
+            Assert.Single(result.Value);
+        }
+
+        // --- GET BY ID ---
+        [Fact]
+        public async Task GetById_ShouldReturnOk_WhenFound()
+        {
+            var dto = CreateDTO();
+            _serviceMock.Setup(s => s.GetByIdAsync(It.IsAny<PhysicalResourceId>()))
+                .ReturnsAsync(dto);
+
+            var result = await _controller.GetByID(Guid.NewGuid());
+
+            Assert.NotNull(result.Value);
+            Assert.Equal(dto.Description, result.Value.Description);
+        }
+
+        [Fact]
+        public async Task GetById_ShouldReturnNotFound_WhenMissing()
+        {
+            _serviceMock.Setup(s => s.GetByIdAsync(It.IsAny<PhysicalResourceId>()))
+                .ReturnsAsync((PhysicalResourceDTO)null!);
+
+            var result = await _controller.GetByID(Guid.NewGuid());
+
+            var notFound = Assert.IsType<NotFoundObjectResult>(result.Result);
+            Assert.Equal(404, notFound.StatusCode);
+        }
+
+        // --- POST CREATE ---
+        [Fact]
+        public async Task Create_ShouldReturnCreated_WhenValid()
+        {
+            var dto = new CreatingPhysicalResourceDTO("Truck A", 25.0, 10.0, PhysicalResourceType.Truck, Guid.NewGuid());
+            var created = CreateDTO();
+
+            _serviceMock.Setup(s => s.AddAsync(dto)).ReturnsAsync(created);
+
+            var result = await _controller.Create(dto);
+
+            var createdResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+            Assert.IsType<PhysicalResourceDTO>(createdResult.Value);
+        }
+
+        [Fact]
+        public async Task Create_ShouldReturnBadRequest_WhenBusinessRuleException()
+        {
+            var dto = new CreatingPhysicalResourceDTO("Truck A", 25.0, 10.0, PhysicalResourceType.Truck, Guid.NewGuid());
+            _serviceMock.Setup(s => s.AddAsync(dto))
+                .ThrowsAsync(new BusinessRuleValidationException("Invalid Qualification"));
+
+            var result = await _controller.Create(dto);
+
+            var badRequest = Assert.IsType<BadRequestObjectResult>(result.Result);
+            Assert.Equal(400, badRequest.StatusCode);
+        }
+
+        // --- PATCH UPDATE ---
+        [Fact]
+        public async Task Update_ShouldReturnOk_WhenValidFields()
+        {
+            var updated = CreateDTO();
+            _serviceMock.Setup(s => s.UpdateAsync(It.IsAny<PhysicalResourceId>(), It.IsAny<UpdatingPhysicalResource>()))
+                .ReturnsAsync(updated);
+
+            var body = new JObject { ["description"] = "Updated" };
+
+            var result = await _controller.Update(Guid.NewGuid(), body);
+
+            var ok = Assert.IsType<OkObjectResult>(result.Result);
+            Assert.IsType<PhysicalResourceDTO>(ok.Value);
+        }
+
+        [Fact]
+        public async Task Update_ShouldReturnBadRequest_WhenInvalidField()
+        {
+            var body = new JObject { ["invalidField"] = "x" };
+
+            var result = await _controller.Update(Guid.NewGuid(), body);
+
+            var badRequest = Assert.IsType<BadRequestObjectResult>(result.Result);
+            Assert.Contains("cannot be updated", badRequest.Value!.ToString());
+        }
+
+        [Fact]
+        public async Task Update_ShouldReturnNotFound_WhenServiceReturnsNull()
+        {
+            _serviceMock.Setup(s => s.UpdateAsync(It.IsAny<PhysicalResourceId>(), It.IsAny<UpdatingPhysicalResource>()))
+                .ReturnsAsync((PhysicalResourceDTO)null!);
+
+            var body = new JObject { ["description"] = "Updated" };
+            var result = await _controller.Update(Guid.NewGuid(), body);
+
+            Assert.IsType<NotFoundResult>(result.Result);
+        }
+
+        [Fact]
+        public async Task Update_ShouldReturnBadRequest_WhenServiceThrows()
+        {
+            _serviceMock.Setup(s => s.UpdateAsync(It.IsAny<PhysicalResourceId>(), It.IsAny<UpdatingPhysicalResource>()))
+                .ThrowsAsync(new BusinessRuleValidationException("Error"));
+
+            var body = new JObject { ["description"] = "Updated" };
+            var result = await _controller.Update(Guid.NewGuid(), body);
+
+            var badRequest = Assert.IsType<BadRequestObjectResult>(result.Result);
+            Assert.Equal(400, badRequest.StatusCode);
+        }
+
+        // --- DEACTIVATE ---
+        [Fact]
+        public async Task Deactivate_ShouldReturnOk_WhenSuccess()
+        {
+            var dto = CreateDTO();
+            _serviceMock.Setup(s => s.DeactivationAsync(It.IsAny<PhysicalResourceId>()))
+                .ReturnsAsync(dto);
+
+            var result = await _controller.Deactivate(Guid.NewGuid());
+
+            Assert.NotNull(result.Value);
+            Assert.Equal(dto.Id, result.Value.Id);
+        }
+
+        [Fact]
+        public async Task Deactivate_ShouldReturnNotFound_WhenMissing()
+        {
+            _serviceMock.Setup(s => s.DeactivationAsync(It.IsAny<PhysicalResourceId>()))
+                .ReturnsAsync((PhysicalResourceDTO)null!);
+
+            var result = await _controller.Deactivate(Guid.NewGuid());
+
+            var notFound = Assert.IsType<NotFoundObjectResult>(result.Result);
+            Assert.Equal(404, notFound.StatusCode);
+        }
+
+        [Fact]
+        public async Task Deactivate_ShouldReturnBadRequest_WhenThrowsException()
+        {
+            _serviceMock.Setup(s => s.DeactivationAsync(It.IsAny<PhysicalResourceId>()))
+                .ThrowsAsync(new Exception("Error"));
+
+            var result = await _controller.Deactivate(Guid.NewGuid());
+
+            var bad = Assert.IsType<BadRequestObjectResult>(result.Result);
+            Assert.Equal(400, bad.StatusCode);
+        }
+
+        // --- REACTIVATE ---
+        [Fact]
+        public async Task Reactivate_ShouldReturnOk_WhenSuccess()
+        {
+            var dto = CreateDTO();
+            _serviceMock.Setup(s => s.ReactivationAsync(It.IsAny<PhysicalResourceId>()))
+                .ReturnsAsync(dto);
+
+            var result = await _controller.Activate(Guid.NewGuid());
+
+            Assert.NotNull(result.Value);
+            Assert.Equal(dto.Id, result.Value.Id);
+        }
+
+        [Fact]
+        public async Task Reactivate_ShouldReturnNotFound_WhenMissing()
+        {
+            _serviceMock.Setup(s => s.ReactivationAsync(It.IsAny<PhysicalResourceId>()))
+                .ReturnsAsync((PhysicalResourceDTO)null!);
+
+            var result = await _controller.Activate(Guid.NewGuid());
+
+            var notFound = Assert.IsType<NotFoundObjectResult>(result.Result);
+            Assert.Equal(404, notFound.StatusCode);
+        }
+
+        [Fact]
+        public async Task Reactivate_ShouldReturnBadRequest_WhenThrowsException()
+        {
+            _serviceMock.Setup(s => s.ReactivationAsync(It.IsAny<PhysicalResourceId>()))
+                .ThrowsAsync(new Exception("Error"));
+
+            var result = await _controller.Activate(Guid.NewGuid());
+
+            var bad = Assert.IsType<BadRequestObjectResult>(result.Result);
+            Assert.Equal(400, bad.StatusCode);
+        }
     }
 }
