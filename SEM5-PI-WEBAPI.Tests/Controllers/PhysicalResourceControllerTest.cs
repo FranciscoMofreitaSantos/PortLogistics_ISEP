8dcc99192ed8c9f19138f0a2d3c7c6ffd1d8c9f7
Emanuel Almeida <1230839@isep.ipp.pt>
2025-10-17 11:02:31 +0100

    US 2.2.12 - Implementation - Issue #109/#113] PhysicalResourceServiceTest.cs added.

---
+++ SEM5-PI-WEBAPI.Tests/Controllers/PhysicalResourceControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Newtonsoft.Json.Linq;
+using SEM5_PI_WEBAPI.Controllers;
+using SEM5_PI_WEBAPI.Domain.PhysicalResources;
+using SEM5_PI_WEBAPI.Domain.PhysicalResources.DTOs;
+using SEM5_PI_WEBAPI.Domain.Qualifications;
+using SEM5_PI_WEBAPI.Domain.Shared;
+using SEM5_PI_WEBAPI.Domain.ValueObjects;
+
+namespace SEM5_PI_WEBAPI.Tests.Controllers
+{
+    public class PhysicalResourceControllerTests
+    {
+       
+    }
+}
