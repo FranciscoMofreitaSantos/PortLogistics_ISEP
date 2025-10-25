ea189a8b984b0ba248bf0e4678dcfebd06c38a0c
Emanuel Almeida <1230839@isep.ipp.pt>
2025-10-25 10:11:56 +0100

    US 2.2.12 - Implementation - Issue #109/#113] PhysicalResourceMapper added.

---
+++ SEM5-PI-WEBAPI/Domain/PhysicalResources/PhysicalResourceMapper.cs
@@ -0,0 +1,29 @@
+﻿using SEM5_PI_WEBAPI.Domain.PhysicalResources.DTOs;
+using SEM5_PI_WEBAPI.Domain.Qualifications;
+using SEM5_PI_WEBAPI.Domain.Shared;
+using SEM5_PI_WEBAPI.Domain.ValueObjects;
+
+namespace SEM5_PI_WEBAPI.Domain.PhysicalResources
+{
+    public static class PhysicalResourceMapper
+    {
+        public static PhysicalResourceDTO ToDto(EntityPhysicalResource resource)
+        {
+            return new PhysicalResourceDTO(
+                resource.Id.AsGuid(),
+                resource.Code,
+                resource.Description,
+                resource.OperationalCapacity,
+                resource.SetupTime,
+                resource.Type,
+                resource.Status,
+                resource.QualificationID?.AsGuid()
+            );
+        }
+        
+        public static List<PhysicalResourceDTO> ToDtoList(List<EntityPhysicalResource> resources)
+        {
+            return resources.Select(ToDto).ToList();
+        }
+    }
+}
