3cf892d83f4c86a0c2b533c095735c9e21510d85
Jorge Ubaldo <1231274@isep.ipp.pt>
2025-11-01 00:10:41 +0000

    [ARQSI - Implementation 3.1.3- #130 & #187] logout page modification for more intuitive one, and route.tsx modification politicas

--- client-ui/src/pages/Logout.tsx
+++ client-ui/src/pages/Logout.tsx
@@ -1,11 +1,28 @@
 import { useEffect } from "react";
 import { logout } from "../services/auth";
+import { FaSignOutAlt } from "react-icons/fa";
+import "./css/logout.css";
 
 export default function Logout() {
-    useEffect(() => {
-        logout();
-        window.location.href = "/login";
-    }, []);
 
-    return <div>A terminar sessão...</div>;
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      logout();
+      window.location.href = "/";
+    }, 1500); // 1.5s para mostrar a animação
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="logout-container">
+      <div className="logout-card">
+        <FaSignOutAlt className="logout-icon-2" />
+        <h2>A terminar sessão...</h2>
+        <p>Por favor aguarde um momento</p>
+
+        <div className="spinner"></div>
+      </div>
+    </div>
+  );
 }
