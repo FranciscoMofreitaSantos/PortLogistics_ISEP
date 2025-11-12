336c27e74a52330bba03ba7d2774b4a8c646339f
Emanuel Almeida <1230839@isep.ipp.pt>
2025-11-12 19:19:18 +0000

    [US 3.2.3 - Implementation - Issue #140] Small fix.

--- client-ui/src/components/layout/Nav.tsx
+++ client-ui/src/components/layout/Nav.tsx
@@ -38,7 +38,7 @@ export default function Nav({ isOpen }: NavProps) {
 
     const adminMenu = user?.role?.includes(Roles.Administrator)
         ? [
-            { label: t("menu.admin"), path: "/admin", icon: <FiSettings /> },
+            { label: t("menu.admin"), path: "/users", icon: <FiSettings /> },
             { label: t("menu.vvn"), path: "/vvn", icon: <FiFileText /> },
         ]
         : [];
