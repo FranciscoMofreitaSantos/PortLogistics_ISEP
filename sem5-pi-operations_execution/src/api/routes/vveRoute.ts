ffd45db1c7d9f63006d77e396587dd3da444e0a1
SAEmanuel <1230839@isep.ipp.pt>
2025-12-18 20:32:36 +0000

    [Implementation - Issue #262, #215] ARQSI US 4.1.7 - Added vveRoute.ts

---
+++ sem5-pi-operations_execution/src/api/routes/vveRoute.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { Container } from "typedi";
+import { celebrate, Joi } from "celebrate";
+import config from "../../config";
+import middlewares from "../middlewares";
+import CreateVVEController from "../../controllers/vve/createVVEController";
+import GetAllVVEController from "../../controllers/vve/getAllVVEController";
+
+const route = Router();
+
+export default (app: Router) => {
+    app.use("/vve", route);
+
+    const createCtrl = Container.get(config.controllers.vesselVisitExecution.create.name) as CreateVVEController;
+    const getAllCtrl = Container.get(config.controllers.vesselVisitExecution.getAll.name) as GetAllVVEController;
+    
+    route.post(
+        "/",
+        celebrate({
+            body: Joi.object({
+                vvnId: Joi.string().required(),
+                actualArrivalTime: Joi.date().iso().required(),
+                creatorEmail: Joi.string().email().optional()
+            })
+        }),
+        (req, res) => createCtrl.execute(req, res)
+    );
+
+    route.get("/", (req, res) => getAllCtrl.execute(req, res));
+};
